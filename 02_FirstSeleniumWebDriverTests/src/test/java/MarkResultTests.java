760f8327baa0c28c0010f26040b984d290f7f572
Aidar <aiid2492@gmail.com>
2023-07-20 10:32:48 +0600

    modul 2.4(1)

---
+++ 02_FirstSeleniumWebDriverTests/src/test/java/MarkResultTests.java
@@ -0,0 +1,80 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MarkResultTests {
+        @Test
+        public  void testRaven2()
+        {
+            var actualResult = getMarkResult(10);
+            Assert.assertEquals("2", actualResult);
+
+        }
+        @Test
+        public  void testRaven3()
+        {
+            var actualResult = getMarkResult(55);
+            Assert.assertEquals("3", actualResult);
+
+        }
+        @Test
+        public  void testRaven4()
+        {
+            var actualResult = getMarkResult(60);
+            Assert.assertEquals("4", actualResult);
+
+        }
+        @Test
+        public  void testRaven5()
+        {
+            var actualResult = getMarkResult(80);
+            Assert.assertEquals("5", actualResult);
+
+        }
+        @Test
+        public  void testRavenError1()
+        {
+            var actualResult = getMarkResult(72);
+            Assert.assertEquals("5", actualResult);
+
+        }
+        @Test
+        public  void testRavenError2()
+        {
+            var actualResult = getMarkResult(71);
+            Assert.assertEquals("4", actualResult);
+
+        }
+        @Test
+        public  void testRavenError3()
+        {
+            var actualResult = getMarkResult(71);
+            Assert.assertEquals("5", actualResult);
+
+        }
+        @Test
+        public  void testRavenError4()
+        {
+            var actualResult = getMarkResult(-50);
+            Assert.assertEquals("no mark result", actualResult);
+
+        }
+        @Test
+        public  void testRavenError5()
+        {
+            var actualResult = getMarkResult(150);
+            Assert.assertEquals("no mark result", actualResult);
+
+        }
+        private String getMarkResult(Integer mark)
+        {
+            if(mark>=0 && mark <=35) return "2";
+
+            if(mark>35 && mark <=56) return "3";
+
+            if(mark>56 && mark<71) return "4";
+
+            if(mark>72 && mark<100) return "5";
+
+            return "no mark result";
+        }
+}
---
+++ 02_FirstSeleniumWebDriverTests/src/test/java/SiteHelloTest2.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SiteHelloTest2 {
+    @Test
+    public void testsiteHello()
+    {
+        System.setProperty("webdiver.chrome.driver", "drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://lm.skillbox.cc/qa_tester/module01/");
+        driver.findElement(By.name("name")).sendKeys("");
+        driver.findElement(By.className("button")).click();
+        var actualResult = driver.findElement(By.className("start-screen__res")).getText();
+        var expectedResult = "Привет, !";
+        Assert.assertEquals(expectedResult, actualResult);
+        driver.quit();
+
+    }
+}
